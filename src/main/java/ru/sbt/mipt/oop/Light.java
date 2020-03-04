5d76b00d39e4476eab68f420afcf96b1d0aa2384
Kate Klema <43951601+KatyaKlema@users.noreply.github.com>
2020-03-04 17:58:18 +0300

    Update Light.java

--- src/main/java/ru/sbt/mipt/oop/Light.java
+++ src/main/java/ru/sbt/mipt/oop/Light.java
@@ -8,33 +8,18 @@ public class Light {
         this.id = id;
         this.isOn = isOn;
     }
-    //This function get us information - first responsibility
-    /*
+    
     public boolean isOn() {
         return isOn;
     }
-    */
-    //this function get us information too
-    /*
+    
+    
     public String getId() {
         return id;
     }
-    */
-
-    //this function set up data for out clas Light - second responsibility
+    
     public void setOn(boolean on) {
         isOn = on;
     }
 }
 
-// We need special class that has main responsibility - to get information (SRP)
-public class LightInformation extends Light{
-    public boolean isOn() {
-        return isOn;
-    }
-    
-    public String getId() {
-        return id;
-    }
-    
-}
