939bc1f7bfe16fc9ab0f6cde37baa43e9b3447e5
Kate Klema <43951601+KatyaKlema@users.noreply.github.com>
2020-03-04 10:20:48 +0300

    Update SmartHome.java

--- src/main/java/ru/sbt/mipt/oop/SmartHome.java
+++ src/main/java/ru/sbt/mipt/oop/SmartHome.java
@@ -13,12 +13,20 @@ public class SmartHome {
     public SmartHome(Collection<Room> rooms) {
         this.rooms = rooms;
     }
-
+    //this function has responsibility to change information about class
     public void addRoom(Room room) {
         rooms.add(room);
     }
-
+    //this function has second responsibility to et information about class
+    /*
     public Collection<Room> getRooms() {
         return rooms;
     }
+    */
+}
+//we need other class to separate responsibility about getting information (SRP)
+public class SmartHomeInformation extends SmartHome{
+    public Collection<Room> getRooms() {
+        return rooms;
+    }  
 }
