2cc89f7be7a8f98452e83a5a242c0e1880dde71d
Kate Klema <43951601+KatyaKlema@users.noreply.github.com>
2020-03-05 01:21:42 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/Door.java
+++ src/main/java/ru/sbt/mipt/oop/Door.java
@@ -8,16 +8,12 @@ public class Door {
         this.isOpen = isOpen;
         this.id = id;
     }
-    
+
     public String getId() {
         return id;
     }
-    
+
     public void setOpen(boolean open) {
         isOpen = open;
     }
-    
 }
-
-
-
