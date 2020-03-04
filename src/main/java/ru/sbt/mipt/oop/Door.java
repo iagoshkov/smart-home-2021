a3ef75902030994de4d81de243c7e87ccef1a40e
Kate Klema <43951601+KatyaKlema@users.noreply.github.com>
2020-03-04 17:57:46 +0300

    Update Door.java

--- src/main/java/ru/sbt/mipt/oop/Door.java
+++ src/main/java/ru/sbt/mipt/oop/Door.java
@@ -8,26 +8,16 @@ public class Door {
         this.isOpen = isOpen;
         this.id = id;
     }
-    /* It is first responsibility: 
-    to get information about Door => 
-    It is better to create new class
-    "DoorInformation" with this 
-    responsibility
+    
     public String getId() {
         return id;
     }
-    */
-    //It is second responsibility: to set up information about Door
+    
     public void setOpen(boolean open) {
         isOpen = open;
     }
     
 }
-//We need special class that has main responsibility - to get information 
-public class DoorInformation extends Door{
-    public String getId() {
-        return id;
-    }
-}
+
 
 
