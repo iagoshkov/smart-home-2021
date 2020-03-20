531728d9e073d29ca9a4520e1e25223f222831bb
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-20 23:38:50 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/SensorEvent.java
+++ src/main/java/ru/sbt/mipt/oop/SensorEvent.java
@@ -1,16 +1,22 @@
 package ru.sbt.mipt.oop;
 
 public class SensorEvent {
-    private final SensorEventType type;
+    private final SensorEventTypeDoor typeDoor;
+    private final SensorEventTypeLight typeLight;
     private final String objectId;
 
-    public SensorEvent(SensorEventType type, String objectId) {
-        this.type = type;
+    public SensorEvent(SensorEventTypeDoor _typeDoor, SensorEventTypeLight _typeLight, String objectId) {
+        this.typeDoor = _typeDoor;
+        this.typeLight = _typeLight;
         this.objectId = objectId;
     }
 
-    public SensorEventType getType() {
-        return type;
+    public SensorEventTypeLight getTypeLight() {
+        return typeLight;
+    }
+
+    public SensorEventTypeDoor getTypeDoor(){
+        return typeDoor;
     }
 
     public String getObjectId() {
@@ -19,8 +25,9 @@ public class SensorEvent {
 
     @Override
     public String toString() {
+
         return "SensorEvent{" +
-                "type=" + type +
+                "type=" + (typeDoor == null ? typeLight : typeDoor) +
                 ", objectId='" + objectId + '\'' +
                 '}';
     }
