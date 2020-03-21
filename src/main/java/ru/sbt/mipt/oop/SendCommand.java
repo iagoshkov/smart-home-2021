3e215b8c73dc8f78de1ac1aa320ac58dd02259a7
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-21 17:03:02 +0800

    Add files via upload

---
+++ src/main/java/ru/sbt/mipt/oop/SendCommand.java
@@ -0,0 +1,7 @@
+package ru.sbt.mipt.oop;
+
+public class SendCommand {
+    public SendCommand(SensorCommand command){
+        System.out.println("Pretent we're sending command " + command);
+    }
+}
