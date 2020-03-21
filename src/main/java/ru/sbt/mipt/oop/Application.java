13800e2a3d7d380891deb6caa434fe6b1ac4ae4f
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-21 16:56:49 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -2,6 +2,8 @@ package ru.sbt.mipt.oop;
 
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Application {
     public static void main(String... args) throws IOException {
@@ -12,9 +14,15 @@ public class Application {
 
         // начинаем цикл обработки событий
         Event eventTemp = new Event();
+        LightIterator lightIterator = new LightIterator(smartHome);
+        DoorIterator doorIterator = new DoorIterator(smartHome);
+        List<Processor> processors = Arrays.asList(new LightEventProcessor(smartHome, lightIterator), new DoorEventProcessor(smartHome, doorIterator));
+
         while (eventTemp.getEvent() != null) {
-            PrintSmartHome tempPrinter = new PrintSmartHome();
-            tempPrinter.print(smartHome, eventTemp);
+            System.out.println("Got event: " + eventTemp.getEvent());
+            for(Processor processor : processors){
+                processor.processing(eventTemp);
+            }
             EventProcessing nextEvent = new EventProcessing();
             eventTemp.setEvent(nextEvent.next(eventTemp.getEvent()));
         }
