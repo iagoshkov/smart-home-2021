ac826d0f3ab4173bdb260d1b301fd97fcd4d2725
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-25 02:05:44 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/PrintLightInformation.java
+++ src/main/java/ru/sbt/mipt/oop/PrintLightInformation.java
@@ -1,10 +1,10 @@
 package ru.sbt.mipt.oop;
 
-import static ru.sbt.mipt.oop.SensorEventTypeLight.LIGHT_ON;
+import static ru.sbt.mipt.oop.SensorEventType.LIGHT_ON;
 
 public class PrintLightInformation {
     public static void printLight(Event event, Light light, Room room){
-        if (event.getEvent().getTypeLight() == LIGHT_ON) {
+        if (event.getSensorEvent().getType() == LIGHT_ON) {
             light.setOn(true);
             System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned on.");
         }
