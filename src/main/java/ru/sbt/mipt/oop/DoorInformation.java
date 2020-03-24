06e86f8fc2159ceead8cc308b3006c924c78a23b
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-25 02:04:40 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/DoorInformation.java
+++ src/main/java/ru/sbt/mipt/oop/DoorInformation.java
@@ -1,10 +1,10 @@
 package ru.sbt.mipt.oop;
 
-import static ru.sbt.mipt.oop.SensorEventTypeDoor.DOOR_OPEN;
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_OPEN;
 
 public class DoorInformation {
     public void toggleDoorLight(Event event, Door door, SmartHome smartHome, Room room){
-        if (event.getEvent().getTypeDoor() == DOOR_OPEN) {
+        if (event.getSensorEvent().getType() == DOOR_OPEN) {
             door.setOpen(true);
             System.out.println("Door " + door.getId() + " in room " + room.getName() + " was opened.");
         } else {
@@ -13,7 +13,7 @@ public class DoorInformation {
             // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
             // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
             HallDoorIterator hallDoorIterator = new HallDoorIterator(smartHome);
-            HallDoorEventProcessor hallDoorEventProcessor = new HallDoorEventProcessor(smartHome, hallDoorIterator);
+            HallDoorEventProcessor hallDoorEventProcessor = new HallDoorEventProcessor(smartHome);
             hallDoorEventProcessor.processing(room);
         }
     }
