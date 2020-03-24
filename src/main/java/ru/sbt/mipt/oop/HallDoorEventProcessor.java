87e96ca2332eb3d4a3c2570142b4d99854c6bf1d
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-25 02:09:03 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/HallDoorEventProcessor.java
+++ src/main/java/ru/sbt/mipt/oop/HallDoorEventProcessor.java
@@ -1,13 +1,16 @@
 package ru.sbt.mipt.oop;
 
-public class HallDoorEventProcessor implements Processor<Room>{
+public class HallDoorEventProcessor implements Processor{
     SmartHome smartHome;
     HallDoorIterator hallDoorIterator;
-    public HallDoorEventProcessor(SmartHome smartHome, HallDoorIterator hallDoorIterator){
+    public HallDoorEventProcessor(SmartHome smartHome){
         this.smartHome = smartHome;
-        this.hallDoorIterator = hallDoorIterator;
+        this.hallDoorIterator = new HallDoorIterator(smartHome);
     }
     @Override
+    public void processing(Event event) {
+
+    }
     public void processing(Room room) {
         if (room.getName().equals("hall")) {
             hallDoorIterator.iterate(room);
