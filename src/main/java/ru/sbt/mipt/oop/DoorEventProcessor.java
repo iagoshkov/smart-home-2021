de97640ae9127cae7a513e064944dfdcdcc68120
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-03-25 02:08:50 +0800

    Add files via upload

--- src/main/java/ru/sbt/mipt/oop/DoorEventProcessor.java
+++ src/main/java/ru/sbt/mipt/oop/DoorEventProcessor.java
@@ -1,23 +1,31 @@
 package ru.sbt.mipt.oop;
 
-import static ru.sbt.mipt.oop.SensorEventTypeDoor.DOOR_CLOSED;
-import static ru.sbt.mipt.oop.SensorEventTypeDoor.DOOR_OPEN;
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_CLOSED;
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_OPEN;
 
-public class DoorEventProcessor implements Processor<Event>{
+public class DoorEventProcessor implements Processor{
     private SmartHome smartHome;
-    private DoorIterator doorIterator;
-    public DoorEventProcessor(SmartHome smartHome, DoorIterator doorIterator){
+    public DoorEventProcessor(SmartHome smartHome){
         this.smartHome = smartHome;
-        this.doorIterator = doorIterator;
     }
     private boolean isDoor(Event event){
-        return event.getEvent().getTypeDoor() == DOOR_OPEN || event.getEvent().getTypeDoor() == DOOR_CLOSED;
+        return event.getSensorEvent().getType() == DOOR_OPEN || event.getSensorEvent().getType() == DOOR_CLOSED;
     }
-    @Override
     public void processing(Event event) {
         if(isDoor(event)){
             // событие от двери
-            doorIterator.iterate(event);
+            smartHome.execute(object -> {
+                if (object instanceof Door) {
+                    Door door = (Door) object;
+                    if (event.getSensorEvent().getObjectId().equals(door.getId())) {
+                        if (event.getSensorEvent().getType() == DOOR_OPEN) {
+                            door.setOpen(true);
+                        } else {
+                            door.setOpen(false);
+                        }
+                    }
+                }
+            });
         }
     }
 }
