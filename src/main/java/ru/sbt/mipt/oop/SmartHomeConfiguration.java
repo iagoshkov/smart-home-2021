73b96b34c3e15b8b0030435a0a2ca7978e2e51a2
isaeva anna <pangolin-666@mail.ru>
2020-03-31 01:08:04 +0300

    fix remarks

--- src/main/java/ru/sbt/mipt/oop/SmartHomeConfiguration.java
+++ src/main/java/ru/sbt/mipt/oop/SmartHomeConfiguration.java
@@ -7,6 +7,7 @@ import ru.sbt.mipt.oop.adapters.SensorEventHandlerAdapter;
 import ru.sbt.mipt.oop.command.ProvisionalSensorCommandSender;
 import ru.sbt.mipt.oop.command.SensorCommandSender;
 import ru.sbt.mipt.oop.decorator.SecurityDecorator;
+import ru.sbt.mipt.oop.event.SensorEventType;
 import ru.sbt.mipt.oop.eventhandler.EventDoorHandler;
 import ru.sbt.mipt.oop.eventhandler.EventHallDoorHandler;
 import ru.sbt.mipt.oop.eventhandler.EventHandler;
@@ -18,6 +19,7 @@ import ru.sbt.mipt.oop.storage.HomeConditionStorage;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 
 @Configuration
@@ -35,19 +37,33 @@ public class SmartHomeConfiguration {
         return smartHome;
     }
 
+    @Bean SensorCommandSender commandSender() {
+        return new ProvisionalSensorCommandSender();
+    }
+
     @Bean
-    public SensorEventsManager sensorEventsManager(SmartHome smartHome) {
+    public List<EventHandler> eventHandlers(SmartHome smartHome, SensorCommandSender commandSender) {
+        return Arrays.asList(
+                new EventDoorHandler(smartHome),
+                new EventLightHandler(smartHome),
+                new EventHallDoorHandler(smartHome, commandSender)
+        );
+    }
+
+    @Bean
+    public Map<String, SensorEventType> convertType(){
+        return Map.of(
+                "LightIsOn", SensorEventType.LIGHT_ON,
+                "LightIsOff", SensorEventType.LIGHT_OFF,
+                "DoorIsOpen", SensorEventType.DOOR_OPEN,
+                "DoorIsClosed", SensorEventType.DOOR_CLOSED
+        );
+    }
+
+    @Bean
+    public SensorEventsManager sensorEventsManager(SmartHome smartHome, SensorCommandSender commandSender) {
         SensorEventsManager sensorEventsManager = new SensorEventsManager();
-
-        SensorCommandSender commandSender = new ProvisionalSensorCommandSender();
-
-        List<EventHandler> eventHandlers = Arrays.asList(new EventDoorHandler(smartHome),
-                                                         new EventLightHandler(smartHome),
-                                                         new EventHallDoorHandler(smartHome, commandSender));
-
-        sensorEventsManager.registerEventHandler(new SensorEventHandlerAdapter(new SecurityDecorator(eventHandlers,
-                                                                                smartHome.getSignaling())));
-
+        sensorEventsManager.registerEventHandler(new SensorEventHandlerAdapter(new SecurityDecorator(eventHandlers(smartHome, commandSender), smartHome.getSignaling()), convertType()));
         return sensorEventsManager;
     }
 }
--- src/main/java/ru/sbt/mipt/oop/adapters/SensorEventHandlerAdapter.java
+++ src/main/java/ru/sbt/mipt/oop/adapters/SensorEventHandlerAdapter.java
@@ -5,16 +5,20 @@ import ru.sbt.mipt.oop.event.SensorEvent;
 import ru.sbt.mipt.oop.event.SensorEventType;
 import ru.sbt.mipt.oop.eventhandler.EventHandler;
 
+import java.util.Map;
+
 public class SensorEventHandlerAdapter implements com.coolcompany.smarthome.events.EventHandler {
     private final EventHandler eventHandler;
+    private final Map<String, SensorEventType> convertTypeMap;
 
-    public SensorEventHandlerAdapter(EventHandler eventHandler) {
+    public SensorEventHandlerAdapter(EventHandler eventHandler, Map<String, SensorEventType> convertTypeMap) {
         this.eventHandler = eventHandler;
+        this.convertTypeMap = convertTypeMap;
     }
 
     @Override
     public void handleEvent(CCSensorEvent event) {
-        SensorEventType type = ConverterTypes.convertType.get(event.getEventType());
+        SensorEventType type = convertTypeMap.get(event.getEventType());
         if (type == null) return;
         eventHandler.handleEvent(new SensorEvent(type, event.getObjectId()));
     }
