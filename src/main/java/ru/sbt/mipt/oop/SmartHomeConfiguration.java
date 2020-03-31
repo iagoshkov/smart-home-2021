6bb8632d345cb67b099c00b8217d0f7be964d029
isaeva anna <pangolin-666@mail.ru>
2020-04-01 00:40:50 +0300

    add RemoteControlRegistry in configuration

--- src/main/java/ru/sbt/mipt/oop/SmartHomeConfiguration.java
+++ src/main/java/ru/sbt/mipt/oop/SmartHomeConfiguration.java
@@ -3,6 +3,8 @@ package ru.sbt.mipt.oop;
 import com.coolcompany.smarthome.events.SensorEventsManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import rc.RemoteControl;
+import rc.RemoteControlRegistry;
 import ru.sbt.mipt.oop.adapters.SensorEventHandlerAdapter;
 import ru.sbt.mipt.oop.command.ProvisionalSensorCommandSender;
 import ru.sbt.mipt.oop.command.SensorCommandSender;
@@ -13,6 +15,7 @@ import ru.sbt.mipt.oop.eventhandler.EventHallDoorHandler;
 import ru.sbt.mipt.oop.eventhandler.EventHandler;
 import ru.sbt.mipt.oop.eventhandler.EventLightHandler;
 import ru.sbt.mipt.oop.objects.SmartHome;
+import ru.sbt.mipt.oop.rc.*;
 import ru.sbt.mipt.oop.signaling.Signaling;
 import ru.sbt.mipt.oop.storage.HomeConditionGsonStorage;
 import ru.sbt.mipt.oop.storage.HomeConditionStorage;
@@ -74,6 +77,67 @@ public class SmartHomeConfiguration {
                 "DoorIsOpen", SensorEventType.DOOR_OPEN,
                 "DoorIsClosed", SensorEventType.DOOR_CLOSED
         );
+    }
+
+    @Bean
+    public String code() {
+        return "12345";
+    }
+
+    @Bean
+    public Command activateSignalingCommand(SmartHome smartHome, String code) {
+        return new ActivateSignalingCommand(smartHome, code);
+    }
+
+    @Bean
+    public Command closeHallDoorCommand(SmartHome smartHome) {
+        return new CloseHallDoorCommand(smartHome);
+    }
+
+    @Bean
+    public Command turnOnHallLightCommand(SmartHome smartHome) {
+        return new TurnOnHallLightCommand(smartHome);
+    }
+
+    @Bean
+    public Command turnOnAlarmCommand(SmartHome smartHome) {
+        return new TurnOnAlarmCommand(smartHome);
+    }
+
+    @Bean
+    public Command turnOnLightCommand(SmartHome smartHome) {
+        return new TurnOnLightCommand(smartHome);
+    }
+
+    @Bean
+    public Command turnOffLightCommand(SmartHome smartHome) {
+        return new TurnOffLightCommand(smartHome);
+    }
+
+    @Bean
+    public RemoteControl remoteControl(Command turnOffLightCommand, Command turnOnLightCommand,
+                                       Command turnOnAlarmCommand, Command turnOnHallLightCommand,
+                                       Command activateSignalingCommand, Command closeHallDoorCommand) {
+        SmartRemoteControl remoteControl = new SmartRemoteControl();
+        remoteControl.set("A", turnOffLightCommand);
+        remoteControl.set("B", turnOnLightCommand);
+        remoteControl.set("C", turnOnAlarmCommand);
+        remoteControl.set("D", turnOnHallLightCommand);
+        remoteControl.set("1", activateSignalingCommand);
+        remoteControl.set("2", closeHallDoorCommand);
+        return remoteControl;
+    }
+
+    @Bean
+    public String rcId() {
+        return "newRemoteControl";
+    }
+
+    @Bean
+    public RemoteControlRegistry remoteControlRegistry(RemoteControl remoteControl, String rcId) {
+        RemoteControlRegistry remoteControlRegistry = new RemoteControlRegistry();
+        remoteControlRegistry.registerRemoteControl(remoteControl, rcId);
+        return remoteControlRegistry;
     }
 
     @Bean
