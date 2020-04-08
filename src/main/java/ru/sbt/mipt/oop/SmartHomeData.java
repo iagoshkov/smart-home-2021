270775eb5e1c3e8864c77414b44726f86a4b0f90
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-04-09 00:00:20 +0300

    Delete SmartHomeData.java

--- src/main/java/ru/sbt/mipt/oop/SmartHomeData.java
+++
@@ -1,9 +0,0 @@
-package ru.sbt.mipt.oop;
-
-import java.io.IOException;
-
-public interface SmartHomeData<T> {
-    public T getData() throws IOException;
-
-    public void writeData(String addr) throws IOException;
-}
