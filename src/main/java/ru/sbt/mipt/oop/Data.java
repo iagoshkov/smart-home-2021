8811bb0b9abc290deaa1ee19eda92e4d80903234
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-04-08 23:52:22 +0300

    Delete Data.java

--- src/main/java/ru/sbt/mipt/oop/Data.java
+++
@@ -1,9 +0,0 @@
-package ru.sbt.mipt.oop;
-
-import java.io.IOException;
-
-public interface Data<T> {
-    public T getData() throws IOException;
-
-    public void writeData(String addr) throws IOException;
-}
