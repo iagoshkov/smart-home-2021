04baa2afce1deae0e5aaa8c595b3c61d20aa9776
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-05-02 16:20:11 +0300

    Update HomeReader.java

--- src/main/java/ru/sbt/mipt/oop/home_readers/HomeReader.java
+++ src/main/java/ru/sbt/mipt/oop/home_readers/HomeReader.java
@@ -2,9 +2,7 @@ package ru.sbt.mipt.oop.home_readers;
 
 import ru.sbt.mipt.oop.home_components.SmartHome;
 
-import java.io.IOException;
-
 public interface HomeReader {
-    public SmartHome read(String addr) throws IOException;
+    public SmartHome read(String addr);
 }
 
