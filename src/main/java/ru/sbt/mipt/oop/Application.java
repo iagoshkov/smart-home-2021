8bb2393cac97f403d76c376f3ecab4b0be6ed972
Alexandr Zimenkov <zimenkov.shura@mail.ru>
2021-03-10 22:57:32 +0300

    different events processing added

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -20,59 +20,24 @@ public class Application {
         while (event != null) {
             System.out.println("Got event: " + event);
             if (event.getType() == LIGHT_ON || event.getType() == LIGHT_OFF) {
-                // событие от источника света
-                for (Room room : smartHome.getRooms()) {
-                    for (Light light : room.getLights()) {
-                        if (light.getId().equals(event.getObjectId())) {
-                            if (event.getType() == LIGHT_ON) {
-                                light.setOn(true);
-                                System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned on.");
-                            } else {
-                                light.setOn(false);
-                                System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned off.");
-                            }
-                        }
-                    }
-                }
+                LightEventProcessing lightEventProcessing = new LightEventProcessing(event, smartHome);
+                lightEventProcessing.processEvent();
             }
             if (event.getType() == DOOR_OPEN || event.getType() == DOOR_CLOSED) {
-                // событие от двери
-                for (Room room : smartHome.getRooms()) {
-                    for (Door door : room.getDoors()) {
-                        if (door.getId().equals(event.getObjectId())) {
-                            if (event.getType() == DOOR_OPEN) {
-                                door.setOpen(true);
-                                System.out.println("Door " + door.getId() + " in room " + room.getName() + " was opened.");
-                            } else {
-                                door.setOpen(false);
-                                System.out.println("Door " + door.getId() + " in room " + room.getName() + " was closed.");
-                                // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
-                                // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
-                                if (room.getName().equals("hall")) {
-                                    for (Room homeRoom : smartHome.getRooms()) {
-                                        for (Light light : homeRoom.getLights()) {
-                                            light.setOn(false);
-                                            SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
-                                            sendCommand(command);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                DoorEventProcessing doorEventProcessing = new DoorEventProcessing(event, smartHome);
+                doorEventProcessing.processEvent();
             }
             event = getNextSensorEvent();
         }
     }
 
     private static void sendCommand(SensorCommand command) {
-        System.out.println("Pretent we're sending command " + command);
+        System.out.println("Pretend we're sending command " + command);
     }
 
     private static SensorEvent getNextSensorEvent() {
         // pretend like we're getting the events from physical world, but here we're going to just generate some random events
-        if (Math.random() < 0.05) return null; // null means end of event stream
+        if (Math.random() < 0.05) { return null; } // null means end of event stream
         SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
         String objectId = "" + ((int) (10 * Math.random()));
         return new SensorEvent(sensorEventType, objectId);
---
+++ src/main/java/ru/sbt/mipt/oop/LightEventProcessing.java
@@ -0,0 +1,30 @@
+package ru.sbt.mipt.oop;
+
+import static ru.sbt.mipt.oop.SensorEventType.LIGHT_ON;
+
+public class LightEventProcessing implements EventProcessing {
+    private final SensorEvent event;
+    private final SmartHome smartHome;
+
+    LightEventProcessing(SensorEvent event, SmartHome smartHome) {
+        this.event = event;
+        this.smartHome = smartHome;
+    }
+
+    public void processEvent() {
+        // событие от источника света
+        for (Room room : smartHome.getRooms()) {
+            for (Light light : room.getLights()) {
+                if (light.getId().equals(event.getObjectId())) {
+                    if (event.getType() == LIGHT_ON) {
+                        light.setOn(true);
+                        System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned on.");
+                    } else {
+                        light.setOn(false);
+                        System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned off.");
+                    }
+                }
+            }
+        }
+    }
+}
