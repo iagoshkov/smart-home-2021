dc3ab3de71a014683bc8b1bfc105452088430287
Alexey Davletshin <naylrush@gmail.com>
2021-03-11 15:33:50 +0300

    Extract EventGenerator

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -17,16 +17,13 @@ public class Application {
         // SRP
         SmartHomeEventHandler smartHomeEventHandler = new SmartHomeEventHandler(smartHome, eventProcessors);
 
-        for (SensorEvent event = getNextSensorEvent(); event != null; event = getNextSensorEvent()) {
+        SensorEvent event;
+        while (true) {
+            event = EventGenerator.getNextSensorEvent();
+            if (event == null) break;
+
             smartHomeEventHandler.handleEvent(event);
         }
     }
 
-    private static SensorEvent getNextSensorEvent() {
-        // pretend like we're getting the events from physical world, but here we're going to just generate some random events
-        if (Math.random() < 0.05) return null; // null means end of event stream
-        SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
-        String objectId = "" + ((int) (10 * Math.random()));
-        return new SensorEvent(sensorEventType, objectId);
-    }
 }
