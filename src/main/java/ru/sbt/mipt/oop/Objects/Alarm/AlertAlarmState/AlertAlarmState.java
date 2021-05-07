8c727b34a9f5354187228a6b4d32e0bfbc0d041e
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-07 15:47:10 +0300

    Create AlertAlarmState.java

---
+++ src/main/java/ru/sbt/mipt/oop/Objects/Alarm/AlertAlarmState/AlertAlarmState.java
@@ -0,0 +1,27 @@
+package ru.sbt.mipt.oop.Objects.Alarm;
+
+public class AlertAlarmState implements AlarmState {
+    private String code;
+
+    public AlertAlarmState(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public AlarmState activate(String code) {
+        return this;
+    }
+
+    @Override
+    public AlarmState deactivate(String code) {
+        if (code.equals(this.code)) {
+            return new DeactivatedAlarmState();
+        }
+        return this;
+    }
+
+    @Override
+    public AlarmState alert() {
+        return this;
+    }
+}
