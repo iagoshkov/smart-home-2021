fb851a584c94c4399195527bb681153345882f94
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-07 15:44:14 +0300

    Create AlarmSensorEventHandler.java

---
+++ src/main/java/ru/sbt/mipt/oop/Handler/Decorator/AlarmSensorEventHandler.java
@@ -0,0 +1,27 @@
+package ru.sbt.mipt.oop.Handler.Decorator;
+import java.util.List;
+import ru.sbt.mipt.oop.Event.SensorEvent;
+import ru.sbt.mipt.oop.Handler.SensorEventHandler;
+import ru.sbt.mipt.oop.Objects.Alarm.SmartAlarm;
+
+public class AlarmSensorEventHandler implements SensorEventHandler {
+    private final SmartAlarm alarm;
+    private final List<SensorEventHandler> handlers;
+
+    public AlarmSensorEventHandler(SmartAlarm alarm, List<SensorEventHandler> handlers) {
+        this.alarm = alarm;
+        this.handlers = handlers;
+    }
+
+    @Override
+    public void handleEvent(SensorEvent event) {
+        if (alarm.isActivated()) {
+            alarm.alert();
+            System.out.println("Sending sms");
+        } else if (alarm.isDeactivated()) {
+            for (SensorEventHandler handler : handlers) {
+                handler.handleEvent(event);
+            }
+        }
+    }
+}
