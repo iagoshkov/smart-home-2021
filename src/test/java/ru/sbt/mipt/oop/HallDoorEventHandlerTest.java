b0a270b19832d9f5e4231c881b0a62c272df7515
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-08 10:32:59 +0300

    refill

--- src/test/java/ru/sbt/mipt/oop/HallDoorEventHandlerTest.java
+++ src/test/java/ru/sbt/mipt/oop/HallDoorEventHandlerTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @RunWith(MockitoJUnitRunner.class)
-public class HallDoorHandlerTest {
+public class HallDoorEventHandlerTest {
   
     @Mock
     SmartHome smartHome;
