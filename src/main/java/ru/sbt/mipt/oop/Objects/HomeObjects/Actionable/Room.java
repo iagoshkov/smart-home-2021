a5b1c42ea7235c126e4b4025cc7647d1c9c9b2f7
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-08 09:47:15 +0300

    Create Room.java

---
+++ src/main/java/ru/sbt/mipt/oop/Objects/HomeObjects/Actionable/Room.java
@@ -0,0 +1,42 @@
+package ru.sbt.mipt.oop.Objects.HomeObjects.Actionable;
+import java.util.Collection;
+import ru.sbt.mipt.oop.Action.Action;
+import ru.sbt.mipt.oop.Objects.HomeObjects.*;
+
+public class Room extends HomeObject implements Actionable {
+    private String name;
+    
+    public String getName() {
+        return name;
+    }
+    
+    private Collection<Door> doors;
+    
+    public Collection<Door> getDoors() {
+        return doors;
+    }
+    
+    private Collection<Light> lights;
+    
+    public Collection<Light> getLights() {
+        return lights;
+    }
+
+    public Room(String name, Collection<Door> doors, Collection<Light> lights) {
+        this.name = name;
+        this.doors = doors;
+        this.lights = lights;
+        super(name);   
+    }
+
+    @Override
+    public void execute(Action action) {
+        action.accept(this);
+        for (Door door : doors) {
+            action.accept(door);
+        }
+        for (Light light : lights) {
+            action.accept(light);
+        }
+    }
+}
