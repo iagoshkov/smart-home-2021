47af5fb765512ad9a098ee2d666be66c14ac6a4c
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-08 09:39:17 +0300

    Create Door.java

---
+++ src/main/java/ru/sbt/mipt/oop/Objects/HomeObjects/Door.java
@@ -0,0 +1,20 @@
+
+package ru.sbt.mipt.oop.Objects.HomeObjects;
+
+public class Door extends HomeObject{
+  
+    private boolean isOpen;
+  
+    public boolean isOpen() {
+        return isOpen;
+    }
+      
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    public Door(boolean isOpen, String id) {
+        super(id);
+        this.isOpen = isOpen;
+    }
+}
