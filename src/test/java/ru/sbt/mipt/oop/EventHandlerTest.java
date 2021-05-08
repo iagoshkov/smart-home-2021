948cb9f8524d265aede5e6c17e27a772bb754a9e
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-08 10:39:46 +0300

    refill

--- src/test/java/ru/sbt/mipt/oop/EventHandlerTest.java
+++ src/test/java/ru/sbt/mipt/oop/EventHandlerTest.java
@@ -5,12 +5,12 @@ import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 import org.junit.runner.RunWith;
-import ru.sbt.mipt.oop.actions.Action;
-import ru.sbt.mipt.oop.events.DoorEvent;
-import ru.sbt.mipt.oop.handlers.DoorEventHandler;
-import ru.sbt.mipt.oop.objects.home_objects.Door;
-import ru.sbt.mipt.oop.objects.home_objects.actionable.SmartHome;
-import ru.sbt.mipt.oop.types.DoorEventType;
+import ru.sbt.mipt.oop.Action.Action;
+import ru.sbt.mipt.oop.Event.*;
+import ru.sbt.mipt.oop.Type.*;
+import ru.sbt.mipt.oop.Handler.*;
+import ru.sbt.mipt.oop.Objects.HomeObjects.*;
+import ru.sbt.mipt.oop.Objects.HomeObjects.Actionable.SmartHome;
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -21,12 +21,21 @@ public class DoorEventHandlerTest {
   
     @Mock
     DoorEvent doorEvent;
+  
+    @Mock
+    LightEvent lightEvent;
 
     @InjectMocks
     DoorEventHandler handler;
   
     @InjectMocks
+    LightEventHandler handler;
+  
+    @InjectMocks
     Door door = new Door(true, "0");
+
+    @InjectMocks
+    Light light = new Light("0", true);
 
     @Test
     public void closeTheDoor() {
@@ -56,6 +65,36 @@ public class DoorEventHandlerTest {
         }).when(smartHome).execute(Mockito.any(Action.class));
         handler.handleEvent(doorEvent);
         assertTrue(door.isOpen());
+    }
+  
+      @Test
+    public void turnTheLightOff() {
+        handler = new LightEventHandler(smartHome);
+        handler.handleEvent(lightEvent);
+        Mockito.when(lightEvent.getObjectId()).thenReturn(light.getId());
+        Mockito.when(lightEvent.getLightEventType()).thenReturn(LightEventType.LIGHT_OFF);
+        Mockito.doAnswer((Answer<Void>) invocation -> {
+            Object[] args = invocation.getArguments();
+            ((Action)args[0]).accept(light);
+            return null;
+        }).when(smartHome).execute(Mockito.any(Action.class));
+        handler.handleEvent(lightEvent);
+        assertFalse(light.isOn());
+    }
+
+    @Test
+    public void turnTheLightOn() {
+        handler = new LightEventHandler(smartHome);
+        handler.handleEvent(lightEvent);
+        Mockito.when(lightEvent.getObjectId()).thenReturn(light.getId());
+        Mockito.when(lightEvent.getLightEventType()).thenReturn(LightEventType.LIGHT_ON);
+        Mockito.doAnswer((Answer<Void>) invocation -> {
+            Object[] args = invocation.getArguments();
+            ((Action)args[0]).accept(light);
+            return null;
+        }).when(smartHome).execute(Mockito.any(Action.class));
+        handler.handleEvent(lightEvent);
+        assertTrue(light.isOn());
     }
   
 }
