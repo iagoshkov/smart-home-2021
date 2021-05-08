6ba1447d8a1b2b1d88477298c7fded354ed83da7
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-08 10:53:04 +0300

    refill

--- src/main/java/ru/sbt/mipt/oop/Objects/Alarm/AlarmState.java
+++ src/main/java/ru/sbt/mipt/oop/Objects/Alarm/AlarmState.java
@@ -2,7 +2,8 @@
 package ru.sbt.mipt.oop.Objects.Alarm;
 
 public interface AlarmState {
+    AlarmState alert();
     AlarmState activate(String code);
     AlarmState deactivate(String code);
-    AlarmState alert();
+    
 }
