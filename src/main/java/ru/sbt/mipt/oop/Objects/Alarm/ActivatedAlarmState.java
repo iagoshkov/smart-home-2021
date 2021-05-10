7b7010286378afb53f1060f5c6cd94910d6b9143
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-10 21:45:07 +0300

    final fixed

--- src/main/java/ru/sbt/mipt/oop/Objects/Alarm/ActivatedAlarmState.java
+++ src/main/java/ru/sbt/mipt/oop/Objects/Alarm/ActivatedAlarmState.java
@@ -1,7 +1,7 @@
 package ru.sbt.mipt.oop.Objects.Alarm;
 
 public class ActivatedAlarmState implements AlarmState {
-    private String code;
+    private final String code;
 
     public ActivatedAlarmState(String code) {
         this.code = code;
