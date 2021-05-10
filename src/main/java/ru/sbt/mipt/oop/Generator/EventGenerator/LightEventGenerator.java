115cf77a3b0430badd2a097e54f7cd92f0d4ed3b
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-10 21:27:42 +0300

    fixed

--- src/main/java/ru/sbt/mipt/oop/Generator/EventGenerator/LightEventGenerator.java
+++ src/main/java/ru/sbt/mipt/oop/Generator/EventGenerator/LightEventGenerator.java
@@ -1,12 +1,12 @@
 package ru.sbt.mipt.oop.Generator.EventGenerator;
 import ru.sbt.mipt.oop.Event.*;
-import ru.sbt.mipt.oop.Type.LightEventType;
+import ru.sbt.mipt.oop.Type.EventType;
 
 public class LightEventGenerator implements EventGenerator {
 
     @Override
     public SensorEvent Generate(String objectId) {
-        LightEventType lightEventType = LightEventType.values()[(int) (2 * Math.random())];
+        EventType lightEventType = EventType.values()[(int) (2 * Math.random())];
         return new LightEvent(objectId, lightEventType);
     }
 }
