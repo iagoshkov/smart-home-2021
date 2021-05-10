deefff41b77b2b70c79de3dc83bd7a29bbe96562
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-10 21:27:10 +0300

    fixed

--- src/main/java/ru/sbt/mipt/oop/Generator/EventGenerator/DoorEventGenerator.java
+++ src/main/java/ru/sbt/mipt/oop/Generator/EventGenerator/DoorEventGenerator.java
@@ -1,12 +1,12 @@
 package ru.sbt.mipt.oop.Generator.EventGenerator;
 import ru.sbt.mipt.oop.Event.*;
-import ru.sbt.mipt.oop.Type.DoorEventType;
+import ru.sbt.mipt.oop.Type.EventType;
 
 public class DoorEventGenerator implements EventGenerator {
 
     @Override
     public SensorEvent Generate(String objectId) {
-        DoorEventType doorEventType = DoorEventType.values()[(int) (2 * Math.random())];
+        EventType doorEventType = EventType.values()[(int) (2 * Math.random())];
         return new DoorEvent(objectId, doorEventType);
     }
 }
