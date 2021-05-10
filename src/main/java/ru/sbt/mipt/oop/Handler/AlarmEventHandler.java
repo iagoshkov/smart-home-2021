0b7ef5ec05d6e07f442805aab9c557d0fbbcdf26
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-10 21:28:56 +0300

    fixed

--- src/main/java/ru/sbt/mipt/oop/Handler/AlarmEventHandler.java
+++ src/main/java/ru/sbt/mipt/oop/Handler/AlarmEventHandler.java
@@ -1,8 +1,8 @@
 package ru.sbt.mipt.oop.Handler;
 import ru.sbt.mipt.oop.Event.*;
 import ru.sbt.mipt.oop.Objects.Alarm.SmartAlarm;
-import static ru.sbt.mipt.oop.Type.AlarmEventType.ALARM_ACTIVATE;
-import static ru.sbt.mipt.oop.Types.AlarmEventType.ALARM_DEACTIVATE;
+import static ru.sbt.mipt.oop.Type.EventType.ON;
+import static ru.sbt.mipt.oop.Types.EventType.OFF;
 
 public class AlarmEventHandler implements SensorEventHandler{
     private SmartAlarm alarm;
@@ -14,9 +14,9 @@ public class AlarmEventHandler implements SensorEventHandler{
     @Override
     public void handleEvent(SensorEvent event) {
         if (event instanceof AlarmEvent) {
-            if (((AlarmEvent)event).getAlarmEventType() == ALARM_ACTIVATE) {
+            if (((AlarmEvent)event).getAlarmEventType() == ON) {
                 alarm.activate(((AlarmEvent)event).getCode());
-            } else if (((AlarmEvent)event).getAlarmEventType() == ALARM_DEACTIVATE) {
+            } else if (((AlarmEvent)event).getAlarmEventType() == OFF) {
                 alarm.deactivate(((AlarmEvent)event).getCode());
             }
         }
