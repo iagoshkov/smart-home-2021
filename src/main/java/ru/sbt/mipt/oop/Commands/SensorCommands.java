6993275a7fe56f9f37372d443e454fe1661422f0
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-10 21:37:07 +0300

    final fixed

--- src/main/java/ru/sbt/mipt/oop/Commands/SensorCommands.java
+++ src/main/java/ru/sbt/mipt/oop/Commands/SensorCommands.java
@@ -2,8 +2,8 @@ package ru.sbt.mipt.oop.Commands;
 import ru.sbt.mipt.oop.Type.CommandType;
 
 public class SensorCommands {
-    private CommandType type;
-    private String objectId;
+    private final CommandType type;
+    private final String objectId;
 
     public SensorCommand(CommandType type, String objectId) {
         this.type = type;
