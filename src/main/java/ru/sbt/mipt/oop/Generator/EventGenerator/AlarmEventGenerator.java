d80b59967af89a0cc56bf3beea1c5b4b1849a527
enhoker <45732134+enhoker@users.noreply.github.com>
2021-05-10 21:26:37 +0300

    fixed

--- src/main/java/ru/sbt/mipt/oop/Generator/EventGenerator/AlarmEventGenerator.java
+++ src/main/java/ru/sbt/mipt/oop/Generator/EventGenerator/AlarmEventGenerator.java
@@ -1,11 +1,11 @@
 package ru.sbt.mipt.oop.Generator.EventGenerator;
 import ru.sbt.mipt.oop.Event.*;
-import ru.sbt.mipt.oop.Type.AlarmEventType;
+import ru.sbt.mipt.oop.Type.EventType;
 
 public class AlarmEventGenerator implements EventGenerator {
     @Override
     public SensorEvent Generate(String code) {
-        AlarmEventType alarmEventType = AlarmEventType.values()[(int) (2 * Math.random())];
+        EventType alarmEventType = EventType.values()[(int) (2 * Math.random())];
         return new AlarmEvent(alarmEventType, code);
     }
 }
