53904f71d70a55bb36e4b81d949f3b77c0a3a968
gitrbond <bond2000r@mail.ru>
2021-05-10 14:49:18 +0300

    deleted getRooms() and getLights()

--- src/main/java/ru/sbt/mipt/oop/EventHandler/HallDoorEventHandler.java
+++ src/main/java/ru/sbt/mipt/oop/EventHandler/HallDoorEventHandler.java
@@ -4,10 +4,7 @@ import ru.sbt.mipt.oop.CommandSender;
 import ru.sbt.mipt.oop.CommandType;
 import ru.sbt.mipt.oop.SensorCommand;
 import ru.sbt.mipt.oop.SensorEvent;
-import ru.sbt.mipt.oop.SmartHome.Door;
-import ru.sbt.mipt.oop.SmartHome.Light;
-import ru.sbt.mipt.oop.SmartHome.Room;
-import ru.sbt.mipt.oop.SmartHome.SmartHome;
+import ru.sbt.mipt.oop.SmartHome.*;
 
 import static ru.sbt.mipt.oop.SensorEventType.*;
 
@@ -23,25 +20,22 @@ public class HallDoorEventHandler implements EventHandler {
         // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
         // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
         System.out.println("Turning lights off at whole home");
-        if(event.getType().equals(DOOR_CLOSED) && findRoomByDoorId(event.getObjectId()).getName().equals("hall")) {
-            for (Room room : smartHome.getRooms()) {
-                for (Light light : room.getLights()) {
-                    light.setOn(false);
-                    SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
-                    CommandSender.sendCommand(command);
+        if(event.getType().equals(DOOR_CLOSED)) {
+            Action turnAllLightsOff = (object) -> {
+                if (object instanceof Light) {
+                    ((Light)object).setOn(false);
                 }
-            }
-        }
-    }
-
-    private Room findRoomByDoorId(String doorId) {
-        for (Room room : smartHome.getRooms()) {
-            for (Door door : room.getDoors()) {
-                if (door.getId().equals(doorId)) {
-                    return room;
+            };
+            Action turnAllLightsOffIfRoomIsHall = (object) -> {
+                if (object instanceof Room && ((Room)object).getName().equals("hall")) {
+                    Room room = (Room)object;
+                    room.execute(roomObj -> {
+                        if(roomObj instanceof Door && ((Door) roomObj).getId().equals(event.getObjectId())) {
+                            smartHome.execute(turnAllLightsOff);
+                        }
+                    });
                 }
-            }
+            };
         }
-        throw new RuntimeException("Door with id=" + doorId + "is not found in home");
     }
 }
